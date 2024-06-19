6c2d2d02a0f29d048a3bf0707338492467fcae5d
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-18 19:57:55 -0600

    removed unnecessary console.log

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   const[color, setColor] = useState("pink")
   
   const changeColor = () => {
-  const randomKey1 = Math.floor(Math.random(0, 256) * 200)
-  const randomKey2 = Math.floor(Math.random(0, 256) * 200)
-  const randomKey3 = Math.floor(Math.random(0, 256) * 200)
-  console.log(`${randomKey1} ${randomKey2} ${randomKey3}`)  
+  const randomKey1 = Math.floor(Math.random(0, 256) * 255)
+  const randomKey2 = Math.floor(Math.random(0, 256) * 255)
+  const randomKey3 = Math.floor(Math.random(0, 256) * 255)
+  
   setColor(`rgb(${randomKey1} ${randomKey2} ${randomKey3})`)
   }
   
