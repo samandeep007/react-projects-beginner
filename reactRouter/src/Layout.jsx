755dc01b45ee7854a8f1f3b17ab5f51534d28e25
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-29 01:30:56 -0600

    chore: Update react-router-dom configuration in Layout and main files

--- reactRouter/src/Layout.jsx
+++ reactRouter/src/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import { Outlet } from 'react-router-dom'
 
-export default function Layout() {
+export default function Layout() { //Layout component is used to wrap the Header, Footer and Outlet components. Outlet component is used to render the child components of the parent component. Outlet comes from the react-router-dom package.
   return (
     <>
     <Header/>
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -9,7 +9,8 @@ import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
 import Github, { gitHubInfoLoader } from './components/Github/Github.jsx'
-// const router = createBrowserRouter([
+
+// const router = createBrowserRouter([  //createBrowserRouter is used to create a router object. It takes an array of route objects as an argument. Each route object contains a path and an element. The path is the URL path and the element is the component that will be rendered when the path is matched.
 //   {
 //     path: '/',
 //     element: <Layout/>,
@@ -31,14 +32,15 @@ import Github, { gitHubInfoLoader } from './components/Github/Github.jsx'
 // ])
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
+  createRoutesFromElements( //createRoutesFromElements is used to create a router object using elements as an argument. Each route element is a Route component that contains a path and an element.
+
     <Route path='/' element={<Layout/>}>
       <Route path="" element={<Home/>}/>
       <Route path="about" element={<About/>}/>
       <Route path="contact" element={<Contact/>}/>
       <Route path="user/:id" element={<User/>}/>
       <Route
-      loader={gitHubInfoLoader} 
+      loader={gitHubInfoLoader} //loader is used to load data from an external source. It takes a function as an argument that returns a promise. The promise is resolved when the data is loaded. It works similar to the useEffect hook.
       path="github" 
       element={<Github/>}/>
     </Route>
@@ -47,6 +49,7 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+     <RouterProvider router={router}/>
+    {/* RouterProvider is used to provide the router object to the child components. It takes a router object as an argument. */}
   </React.StrictMode>,
 )
